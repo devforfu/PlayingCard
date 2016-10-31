e8c6f71c1e198c669b29d3bbfce4a4b98d63ef46
devforfu <developer.z@outlook.com>
2016-10-31 21:58:49 +0500

    Update PlayingCard.swift

--- Sources/PlayingCard.swift
+++ Sources/PlayingCard.swift
@@ -4,6 +4,11 @@ public struct PlayingCard: CustomStringConvertible {
     let rank: Rank
     let suit: Suit
 
+    public init(rank: Rank, suit: Suit) {
+        self.rank = rank
+        self.suit = suit
+    }
+    
     public var description: String {
         return "\(rank) of \(suit)".capitalized
     }
